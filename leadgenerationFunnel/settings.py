64e8dd2d2eef37aeb627e1ee8bd3c723b6e7fbbf
Sonu Pal <sonupal0840@example.com>
2025-07-09 11:51:36 +0530

    Exclude leads app for senttemplate repo

--- leadgenerationFunnel/settings.py
+++ leadgenerationFunnel/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
+    'senttemplate',
     'django.contrib.sites',
     # 'django_celery_beat',
 ]
