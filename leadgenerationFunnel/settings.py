7687fb39ac913ad4162736af68ed052831f39ff2
Sonu Pal <sonupal0840@example.com>
2025-07-12 10:41:14 +0530

    change database SqLite to PostgreSQL

--- leadgenerationFunnel/settings.py
+++ leadgenerationFunnel/settings.py
@@ -65,11 +65,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'leadgenerationFunnel.wsgi.application'
 
-# Database
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
 
