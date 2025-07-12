d01e816ffab006b08114d35b8daf7f5842ca7a6f
Sonu Pal <sonupal0840@example.com>
2025-07-12 10:56:39 +0530

    change url endpoint

--- senttemplate/urls.py
+++ senttemplate/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("trigger/", views.trigger_template_message, name="trigger_template_message"),
     path("get-templates/", views.get_templates_from_meta, name="get_templates_from_meta"),
-    path("auto/", views.automated_template_from_api, name="automated_template_from_api"),
+    path("", views.automated_template_from_api, name="automated_template_from_api"),
 ]
